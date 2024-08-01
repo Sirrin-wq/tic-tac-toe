f29e3f7098cb9f7338a340b314aa31f119758a75
Yan Bisyukov <yanbisyukov@gmail.com>
2024-08-02 00:23:31 +0300

    add Player.java and TicTacToeManager.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
+        TicTacToeManager ticTacToeManager = new TicTacToeManager();
+        ticTacToeManager.createBoard();
+        ticTacToeManager.printBoard();
     }
 }
+
+/*
+Present menu to user
+  Start the game
+    - create the board
+    - create player 1 and 2
+      - Take player names as user input
+      - Take player symbol as an input
+    - ask player to make a move
+    - check every move for win or draw condition
+    - print the board for every move
+  Exit
+ */
---
+++ src/main/java/TicTacToeManager.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class TicTacToeManager {
+    private char[][] board;
+    private Player playerOne;
+    private Player playerTwo;
+    private Scanner scanner = new Scanner(System.in);
+
+
+    public void start() {
+
+        pickMenuOption();
+
+        // creating the players
+
+        // creating the board
+        // print the board
+        // ask player to move until the game is over
+
+    }
+
+    public Player createPlayer(int playerNum){
+        System.out.println("");
+
+        return null;
+    }
+
+    public void pickMenuOption(){
+        String strInput;
+
+        do {
+            displayMenu();
+
+            // read user input
+            strInput = scanner.nextLine();
+
+            // check the chosen option
+            if (strInput.equals("2")) {
+                System.exit(0);
+            }
+
+        } while (!strInput.equals("1")); //todo make verification
+    }
+
+
+    public void displayMenu() {
+        System.out.println("1. Play\n2. Exit");
+    }
+
+
+    public void createBoard() {
+        board = new char[][]{
+                {'.', '.', '.'},
+                {'.', '.', '.'},
+                {'.', '.', '.'}
+        };
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
