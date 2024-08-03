0698d2e5ba30f53737e3c9e201c5b11da20bfdfe
Faizan <faizanfirdousi.official@gmail.com>
2024-08-03 23:40:08 +0530

    make checkwin() to check winning condition

--- src/main/java/TicTacToeManager.java
+++ src/main/java/TicTacToeManager.java
@@ -1,144 +1,162 @@
 import java.util.Scanner;
 
 public class TicTacToeManager {
-    private char[][] board;
-    private Player playerOne;
-    private Player playerTwo;
-    private Scanner scanner = new Scanner(System.in);
+	private char[][] board;
+	private Player playerOne;
+	private Player playerTwo;
+	private Scanner scanner = new Scanner(System.in);
 
+	public void start() {
 
-    public void start() {
+		pickMenuOption();
+		playerOne = createPlayer(1);
+		playerTwo = createPlayer(2);
 
-        pickMenuOption();
-        playerOne=createPlayer(1);
-        playerTwo=createPlayer(2);
+		// creating the players
 
-        // creating the players
+		// creating the board
+		// print the board
+		// ask player to move until the game is over
 
-        // creating the board
-        // print the board
-        // ask player to move until the game is over
+	}
 
-    }
+	public Player createPlayer(int playerNum) {
+		String playerName;
+		String symbol;
+		do {
+			System.out.print("Player " + playerNum + " name yourself: ");
+			playerName = scanner.nextLine();
+		} while (isEnglishAlphabet(playerName));
 
-    public Player createPlayer(int playerNum) {
-        String playerName;
-        String symbol;
-        do {
-            System.out.print("Player " + playerNum + " name yourself: ");
-            playerName = scanner.nextLine();
-        } while (isEnglishAlphabet(playerName));
+		do {
+			System.out.print("Enter your symbol: ");
+			symbol = scanner.nextLine();
 
-        do {
-            System.out.print("Enter your symbol: ");
-            symbol = scanner.nextLine();
+		} while (isEnglishAlphabet(symbol));
 
-        } while (isEnglishAlphabet(symbol));
+		return new Player(playerName, symbol.charAt(0));
+	}
 
-        return new Player(playerName, symbol.charAt(0));
-    }
+	// Checks that characters are A-Z or a-z
+	public boolean isEnglishAlphabet(String name) {
+		for (char c : name.toCharArray()) {
+			if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    // Checks that characters are A-Z or a-z
-    public boolean isEnglishAlphabet(String name) {
-        for (char c : name.toCharArray()) {
-            if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public void pickMenuOption() {
+		String strInput;
 
-    public void pickMenuOption() {
-        String strInput;
+		do {
+			displayMenu();
 
-        do {
-            displayMenu();
+			// read user input
+			strInput = scanner.nextLine();
 
-            // read user input
-            strInput = scanner.nextLine();
+			// check the chosen option
+			if (strInput.equals("2")) {
+				System.exit(0);
+			}
 
-            // check the chosen option
-            if (strInput.equals("2")) {
-                System.exit(0);
-            }
+		} while (!strInput.equals("1")); // todo make verification
+	}
 
-        } while (!strInput.equals("1")); //todo make verification
-    }
+	public void displayMenu() {
+		System.out.println("1. Play\n2. Exit");
+	}
 
+	public void createBoard() {
+		board = new char[][] { { '.', '.', '.' },
+			                   { '.', '.', '.' },
+			                   { '.', '.', '.' } };
+	}
 
-    public void displayMenu() {
-        System.out.println("1. Play\n2. Exit");
-    }
+	public void printBoard() {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 
+	public void makeMove() {
+		int intRow;
+		String strRow;
+		int intColumn;
+		String strColumn;
+		do {
+			do {
+				System.out.print("Enter the row number: ");
+				strRow = scanner.nextLine();
+				if (isSingleDigit(strRow)) {
+					intRow = Integer.parseInt(strRow) - 1; // user input will be from 1-3 and we'll check from 0-2
+					if (isInBounds(intRow)) {
+						break;
+					}
+				}
+			} while (true);
 
-    public void createBoard() {
-        board = new char[][]{
-                {'.', '.', '.'},
-                {'.', '.', '.'},
-                {'.', '.', '.'}
-        };
-    }
+			do {
+				System.out.print("Enter the column number: ");
+				strColumn = scanner.nextLine();
+				if (isSingleDigit(strColumn)) {
+					intColumn = Integer.parseInt(strColumn) - 1; // user input will be from 1-3 and we'll check from 0-2
+					if (isInBounds(intColumn)) {
+						break;
+					}
+				}
+			} while (true);
+			if (board[intRow][intColumn] == '.') {
+				board[intRow][intColumn] = playerOne.getSymbol();
+				break;
+			} else {
+				System.out.println("The position is already taken");
+			}
+		} while (true);
+	}
 
-    public void printBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+	public boolean isSingleDigit(String input) {
+		if (input.length() == 1 && Character.isDigit(input.charAt(0))) {
+			return true;
+		} else {
+			System.out.println("Input needs to be a single whole digit");
+			return false;
+		}
+	}
 
-    public void makeMove() {
-        int intRow;
-        String strRow;
-        int intColumn;
-        String strColumn;
-        do {
-            do {
-                System.out.print("Enter the row number: ");
-                strRow = scanner.nextLine();
-                if (isSingleDigit(strRow)) {
-                    intRow = Integer.parseInt(strRow)-1; //user input will be from 1-3 and we'll check from 0-2
-                    if (isInBounds(intRow)) {
-                        break;
-                    }
-                }
-            } while (true);
+	public boolean isInBounds(int num) {
+		if (num >= 0 && num < 3) {
+			return true;
+		} else {
+			System.out.println("Input needs to be a positive number");
+			return false;
+		}
+	}
 
-            do {
-                System.out.print("Enter the column number: ");
-                strColumn = scanner.nextLine();
-                if (isSingleDigit(strColumn)) {
-                    intColumn = Integer.parseInt(strColumn)-1; //user input will be from 1-3 and we'll check from 0-2
-                    if (isInBounds(intColumn)) {
-                        break;
-                    }
-                }
-            } while (true);
-            if (board[intRow][intColumn] == '.') {
-                board[intRow][intColumn] = playerOne.getSymbol();
-                break;
-            } else {
-                System.out.println("The position is already taken");
-            }
-        } while (true);
-    }
+	public boolean checkWin(char symbol) {
+		for (int i = 0; i < 3; i++) {
+			if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+				return true;
+			}
 
-    public boolean isSingleDigit(String input) {
-        if (input.length() == 1 && Character.isDigit(input.charAt(0))) {
-            return true;
-        } else {
-            System.out.println("Input needs to be a single whole digit");
-            return false;
-        }
-    }
+			if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+				return true;
+			}
+		}
 
-    public boolean isInBounds(int num) {
-        if (num >= 0 && num<3) {
-            return true;
-        } else {
-            System.out.println("Input needs to be a positive number");
-            return false;
-        }
-    }
+		if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+			return true;
+		}
+		if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+			return true;
+		}
+		
+		return false;
+	}
 }
+
+    
