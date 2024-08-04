2d4ecc864778a52439eeb8fd87004120cbc7815e
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-08-04 10:57:48 -0700

    Takes createBoard() out of main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         TicTacToeManager ticTacToeManager = new TicTacToeManager();
-        ticTacToeManager.createBoard();
+        //ticTacToeManager.createBoard();
         ticTacToeManager.printBoard();
+        ticTacToeManager.start();
     }
 }
 
